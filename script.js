db1d71d8c3b99d5764badee1e5fa182d16fb406c
dudals4657 <kimyoungmin77677279@icloud.com>
2022-01-03 08:59:06 +0900

    스크립트 3번문제 해결중

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // 1. li 불러오기 및 저장, 이벤트 확인, 함수 실행
-const clickLi = document.querySelectorAll(`li`)
-for (let i = 0; i < clickLi.length; i++) {
-    clickLi[i].addEventListener(`click`, function () {
-        alert()
-    })
+// const clickLi = document.querySelectorAll(`li`)
+// for (let i = 0; i < clickLi.length; i++) {
+//     clickLi[i].addEventListener(`click`, function () {
+//         alert()
+//     })
     
-}
+// }
 // 2. input ID 불러오기 및 저장, 이벤트 확인, 함수 실행
 const textInput = document.querySelector(`.add`)
-const button = document.querySelector(`button`)
+const button = document.querySelector(`.addButton`)
 button.addEventListener('click', function () {
     alert(textInput.value)
 })
+//3. li 목록에 삭제 버튼 추가하기, 삭제 버튼 클릭시 몇번째 li인지 alret하기
+const listLi = document.querySelectorAll(`li`)
+const liButton = document.querySelectorAll(`listLi.Button`)
+for (let l = 0; l < listLi.length; l++) {
+    
+    
+}
