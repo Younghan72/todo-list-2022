465ef2064f8abb042ddac77b6170e7d0adef6208
dudals4657 <kimyoungmin77677279@icloud.com>
2022-01-15 14:38:32 +0900

    node.js nodemon설치 밎 정적파일 제공

--- app.js
+++ app.js
@@ -2,17 +2,23 @@
 const express = require('express') // 모듈 , 패키지 . npm  
 const app = express()
 const port = 3000
+app.use(express.static('public')); // 정적 파일 제공 
 
-// 라우트  
-app.get('/', (req, res) => {
-    // http://localhost:3000/ == / 
-  res.send('Hello World!')
-})
+// 라우트, 라우터  
+// app.get('/', (req, res) => {
+//     // http://localhost:3000/ == / 
+//   res.send('Hello World!')
+// })
 
-app.get('/todo', (req, res) => {
-    // res.send("todo list")
-    
-})
+// app.get('/todo', (req, res) => {
+//     // res.send("todo list")
+//     console.log(__dirname)
+//     res.sendFile(__dirname +'/public/index.html') 
+// })
+
+// app.get('/test', (req, res) => {
+//     res.sendFile(`${__dirname}/public/test.html`)
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
